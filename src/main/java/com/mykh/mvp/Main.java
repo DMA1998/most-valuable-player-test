4a3f853fdab12ac3e7cac13dbc269ac133248e94
dmytro.mykh <dmytro.mykh@avid.com>
2024-03-11 16:59:15 +0200

    Init

---
+++ src/main/java/com/mykh/mvp/Main.java
@@ -0,0 +1,12 @@
+package com.mykh.mvp;
+
+/**
+ * @author Dmytro Mykh on 11/03/2024
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+
+}
---
+++ src/test/java/com/mykh/mvp/TestClass.java
@@ -0,0 +1,7 @@
+package com.mykh.mvp;
+
+/**
+ * @author Dmytro Mykh on 11/03/2024
+ */
+public class TestClass {
+}
